c75dd6a16d3800130ca9db38b9357560efb6e831
jovica.pantic <jovica.pantic@boutsourcing.com>
2019-10-25 14:15:53 +0200

    v1.0 - working

--- backend/src/main/kotlin/no/dossier/app/kotlindemo/backend/QuoteGenerator.kt
+++ backend/src/main/kotlin/no/dossier/app/kotlindemo/backend/QuoteGenerator.kt
@@ -1,8 +1,23 @@
 package no.dossier.app.kotlindemo.backend;
 
+import com.beust.klaxon.JsonArray
 import com.beust.klaxon.JsonObject
+import kotlin.random.Random
 
 fun generateQuote(json: JsonObject): String {
- return json["entryPoint"].toString()
+    val entryPoint = json["entryPoint"].toString()
+    val wordClasses = json["wordClasses"] as JsonObject
+
+
+    return recursiveCall(wordClasses, entryPoint).capitalize()
 }
 
+fun recursiveCall(json: JsonObject, key: String): String {
+ val words = json[key] as JsonArray<*>
+ val index = Random.nextInt(words.size)
+ val phrase = words[index].toString()
+
+ return phrase.replace(Regex("<(.+?)>")) {
+  recursiveCall(json, it.groupValues[1])
+ }
+}
